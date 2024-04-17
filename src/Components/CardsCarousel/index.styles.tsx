eed805f5e1a19b87d6867b13b4f4c090ef346f6c
null <null>
2024-04-17 23:36:58 +0530

    responsive view

--- src/Components/CardsCarousel/index.styles.tsx
+++ src/Components/CardsCarousel/index.styles.tsx
@@ -11,11 +11,15 @@ export const CardsContainer = styled.div`
 export const Container = styled.aside`
  display: flex;
  max-width: 1060px;
- flex-wrap: wrap;
  gap: 75px;
  align-items: center;
  justify-content: center;
  padding: 67px 0;
+
+ @media (min-width: 768px) and (max-width: 1024px) {
+    max-width: 100%;
+    justify-content: flex-start;
+ }
 `
 
 export const Content = styled.div`
@@ -25,15 +29,28 @@ export const Content = styled.div`
  max-width: 388px;
  padding: 10px 20px;
  gap: 18px;
+
+ @media (min-width: 768px) and (max-width: 1024px) {
+    max-width: 275px;
+ }
 `;
 
 export const Title = styled.h3`
  font-family: "Jost";
  font-size: 53px;
  line-height: 1.2;
+
+ @media (min-width: 768px) and (max-width: 1024px){
+   font-size: 44px;
+   line-height: 1.1;
+ }
 `;
 
 export const Description = styled.h5`
  font-size: 24px;
  font-weight: 400;
+
+ @media (min-width: 768px) and (max-width: 1024px) {
+    font-size: 18px;
+  }
 `;
